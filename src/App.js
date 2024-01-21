fb85b6b95ce1b6ecba283cd446f9144b8284bade
João Metzdorf <joaovictorlimametzdorf@gmail.com>
2024-01-21 19:39:58 -0300

    Organizando o projeto

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> e salve para recarregar.
-        </p>
-      </header>
     </div>
   );
 }
