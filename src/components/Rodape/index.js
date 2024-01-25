293ebe9ebe40d626e715d73e570e2bd906c03c84
João Metzdorf <joaovictorlimametzdorf@gmail.com>
2024-01-25 19:42:00 -0300

    arrumando nomes das classes do rodape

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -2,8 +2,8 @@ import "./Rodape.css";
 
 const Rodape = (props) => {
   return (
-    <footer className="rodape">
-      <div className="rodape__sociais">
+    <footer className="footer">
+      <div className="footer__sociais">
         <a href="https://www.facebook.com">
           <img src="/images/fb.png" alt="Logo do Facebook" />
         </a>
@@ -15,11 +15,11 @@ const Rodape = (props) => {
         </a>
       </div>
 
-      <div className="rodape__logo">
+      <div className="footer__logo">
         <img src="/images/logo.png" alt="Logo do Organo" />
       </div>
 
-      <div className="rodape__autor">
+      <div className="footer__autor">
         <p>{props.autor}</p>
       </div>
     </footer>
