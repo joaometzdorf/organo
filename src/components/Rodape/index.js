5f573c449fed39d95da5306c91a23e1d9e0b89fa
João Metzdorf <joaovictorlimametzdorf@gmail.com>
2024-01-27 16:58:39 -0300

    target _blank nos links das redes sociais

--- src/components/Rodape/index.js
+++ src/components/Rodape/index.js
@@ -4,13 +4,13 @@ const Rodape = (props) => {
   return (
     <footer className="footer">
       <div className="footer__sociais">
-        <a href="https://github.com/joaometzdorf">
+        <a rel="noopener noreferrer" target="_blank" href="https://github.com/joaometzdorf">
           <img src="/images/github.svg" alt="Logo do GitHub" />
         </a>
-        <a href="https://www.instagram.com/joaometzdorf/">
+        <a rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/joaometzdorf/">
           <img src="/images/ig.png" alt="Logo do Instagram" />
         </a>
-        <a href="https://www.linkedin.com/in/joaometzdorf/">
+        <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/joaometzdorf/">
           <img src="/images/linkedin.svg" alt="Logo do Linkedin" />
         </a>
       </div>
